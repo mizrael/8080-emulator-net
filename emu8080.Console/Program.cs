afd3663d33b1cc2c689d40a07f9b304417bdd105
Davide Guida <davide_guida@dell.com>
2019-10-09 16:28:47 +0200

    added cpudiag support

--- emu8080.Console/Program.cs
+++ emu8080.Console/Program.cs
@@ -117,9 +117,10 @@ namespace emu8080.Console
                 return;
 
             //Fix the first instruction to be JMP 0x100    
-            memory[0] = 0xc3;
-            memory[1] = 0;
-            memory[2] = 0x01;
+             cpu.State.ProgramCounter = 0x100;
+            //memory[0] = 0xc3;
+            //memory[1] = 0;
+            //memory[2] = 0x01;
 
             //Fix the stack pointer from 0x6ad to 0x7ad    
             // this 0x06 byte 112 in the code, which is    
@@ -131,61 +132,78 @@ namespace emu8080.Console
             memory[0x59d] = 0xc2;
             memory[0x59e] = 0x05;
 
-            cpu.ReplaceOpcode(0xcd, (m, c) =>
+            // Inject RET at 0x0005 to handle "CALL 5".
+            memory[5] = 0xC9;
+            cpu.ReplaceOpcode(0xC9, (m, c) =>
             {
-                var hi = m[c.State.ProgramCounter + 2];
-                var lo = m[c.State.ProgramCounter + 1];
-                var value = Utils.GetValue(hi, lo);
-                if (5 == value)
-                {
-                    if (9 == c.State.C)
-                    {
-                        // https://github.com/ezet/i8080-emulator/blob/master/diag/CPUDIAG.DOC
+                byte lo = m[cpu.State.StackPointer];
+                byte hi = m[cpu.State.StackPointer + 1];
+                var ret = Utils.GetValue(hi, lo);
+                var op = m[ret];
 
-                        var sb = new StringBuilder();
-                        var index = c.State.DE + 3;
-                        var letter = (char)m[index];
-                        while (letter != '$')
-                        {
-                            sb.Append(letter);
-                            letter = (char)m[++index];
-                        }
-
-                        var text = sb.ToString();
-                        var isFailure = text.Contains("fail", StringComparison.InvariantCultureIgnoreCase);
-
-                        if (isFailure)
-                        {
-                            letter = (char)m[++index];
-                            while (letter != (char)173) 
-                            {
-                                sb.Append(letter);
-                                letter = (char)m[++index];
-                            } 
-                            text = sb.ToString();
-                        }
-
-                        System.Console.WriteLine(sb);
-
-                        if (isFailure)
-                            throw new Exception(text);
-                    }
-                    else if (2 == c.State.C)
-                    {
-                        System.Console.WriteLine("printing something else");
-                    }
-
-                    c.State.ProgramCounter += 3;
-                }
-                else if (0 == value)
-                {
-                    return;
-                }
-                else
-                {
-                    Ops.CALL(m, c);
-                }
+                var err = $"error at {ret:X} : {op:X} . State: {c.State}";
+                System.Console.WriteLine(err);
+                throw new InvalidProgramException(err);
             });
+
+            //cpu.ReplaceOpcode(0xcd, (m, c) =>
+            //{
+            //    if (m[c.State.ProgramCounter] == 0x76)
+            //    {
+            //        System.Console.WriteLine($"HLT at {c.State.ProgramCounter:X}");
+            //        return;
+            //    }
+
+            //    var hi = m[c.State.ProgramCounter + 2];
+            //    var lo = m[c.State.ProgramCounter + 1];
+            //    var value = Utils.GetValue(hi, lo);
+
+            //    if (0 == value)
+            //    {
+            //        System.Console.WriteLine($"end of the test");
+            //    }
+            //    else if (0x0005 == value)
+            //    {
+            //        if (0x09 == c.State.C)
+            //        {
+            //            // https://github.com/ezet/i8080-emulator/blob/master/diag/CPUDIAG.DOC
+
+            //            var sb = new StringBuilder();
+            //            var index = c.State.DE + 3;
+            //            var letter = (char)m[index];
+            //            while (letter != '$')
+            //            {
+            //                letter = (char)m[++index];
+            //                sb.Append(letter);
+            //            }
+
+            //            System.Console.Write(sb);
+            //        }
+            //        else if (0x02 == c.State.C)
+            //        {
+            //            System.Console.Write((char)c.State.E);
+            //        }
+
+            //        c.State.ProgramCounter += 3;
+            //    }
+            //    else if (0 == c.State.ProgramCounter)
+            //    {
+            //        System.Console.WriteLine($"Jump to 0000 from {c.State.ProgramCounter:X}");
+            //        return;
+            //    }
+            //    else if (0x76 == value)
+            //    {
+            //        System.Console.WriteLine($"HLT at {c.State.ProgramCounter:X}");
+            //        return;
+            //    }
+            //    else
+            //    {
+            //        Ops.CALL(m, c);
+
+            //        if (0 == c.State.ProgramCounter)
+            //            System.Console.WriteLine($"Jump to 0000 from {c.State.ProgramCounter:X}");
+            //    }
+            //});
         }
 
         private static void SaveDump(string dump)
@@ -210,23 +228,33 @@ namespace emu8080.Console
         {
             int i = 0;
             var sb = new StringBuilder();
+
+            var showDebug = false;
             while (true)
             {
-                sb.Append(i++);
-                sb.Append(" ) ");
-                sb.Append(cpu.State);
+                if (!showDebug)
+                {
+                    cpu.Step(memory);
+                    memory.UpdateVideoBuffer();
+                }
+                else
+                {
+                    sb.Append(i++);
+                    sb.Append(" ) ");
+                    sb.Append(cpu.State);
 
-                cpu.Step(memory);
-                memory.UpdateVideoBuffer();
+                    cpu.Step(memory);
+                    memory.UpdateVideoBuffer();
 
-                sb.Append(cpu.State.ProgramCounter.ToString("X"));
-                sb.Append(" : ");
-                sb.Append(memory[cpu.State.ProgramCounter].ToString("X"));
-                sb.Append(memory[cpu.State.ProgramCounter + 1].ToString("X"));
-                sb.Append(memory[cpu.State.ProgramCounter + 2].ToString("X"));
+                    sb.Append(cpu.State.ProgramCounter.ToString("X"));
+                    sb.Append(" : ");
+                    sb.Append(memory[cpu.State.ProgramCounter].ToString("X"));
+                    sb.Append(memory[cpu.State.ProgramCounter + 1].ToString("X"));
+                    sb.Append(memory[cpu.State.ProgramCounter + 2].ToString("X"));
 
-                System.Console.WriteLine(sb.ToString());
-                sb.Clear();
+                    System.Console.WriteLine(sb.ToString());
+                    sb.Clear();
+                }
 
                 if (cancellationToken.IsCancellationRequested)
                     break;
