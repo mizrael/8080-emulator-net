9ab26404b446b1fe95a4f512a8475d02e89e73e4
David Guida <obiuan@gmail.com>
2023-04-03 10:46:33 -0400

    updated video memory handling

--- emu8080.Core/Memory.cs
+++ emu8080.Core/Memory.cs
@@ -1,20 +1,19 @@
-﻿using System.Linq;
+﻿using System;
 
 namespace emu8080.Core
 {
     public class Memory
     {
         private readonly byte[] _bytes;
-        private byte[] _videoBuffer;
-
-        public const ushort videoBufferStartAddress = 0x2400;
-        public const ushort videoBufferEndAddress = 0x4000;
-        public const int videoBufferSize = videoBufferEndAddress - videoBufferStartAddress;
+        private ReadOnlyMemory<byte> _videoBuffer;
+        private const ushort videoBufferStartAddress = 0x2400;
+        private const ushort videoBufferSize = 224 * 256 / 8;
         
         private Memory(byte[] data)
         {
             _bytes = data;
-            _videoBuffer = new byte[videoBufferSize];
+             
+            _videoBuffer = new ReadOnlyMemory<byte>(data, videoBufferStartAddress, videoBufferSize);
         }
 
         public byte this[int index] {
@@ -29,14 +28,10 @@ namespace emu8080.Core
             return Utils.GetValue(hi, lo);
         }
 
-        public byte[] VideoBuffer => _videoBuffer;
+        public ReadOnlyMemory<byte> VideoBuffer => _videoBuffer;
 
-        public void UpdateVideoBuffer()
+        public static Memory Load(byte[] data, int destLoc = 0)
         {
-            System.Buffer.BlockCopy(_bytes, videoBufferStartAddress, _videoBuffer, 0, videoBufferSize);
-        }
-
-        public static Memory Load(byte[] data, int destLoc = 0){
             var destBytes = new byte[0x10000]; // 64kb
             System.Array.Copy(data, 0, destBytes, destLoc, data.Length);
 
